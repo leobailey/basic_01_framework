2d6deb1d15ed1d4430856ef79911d45672d35960
Leo Bailey <leobailey@hotmail.com>
2019-09-17 15:31:39 +0100

    try to get port working

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+var port = process.env.PORT || 3000
+console.log('Server Listening at port ' + port)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
