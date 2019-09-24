f1cdd675e82057c77cff4707f9bc5afb353f1b17
Leo Bailey <leobailey@hotmail.com>
2019-09-24 17:28:42 +0100

    server api routes implemented

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express() // gets handle to express
+const api = require('./api') // collects together all the routes in index.js
+const morgan = require('morgan') //for logging
+const bodyParser = require('body-parser') //for parsing html 
+
+app.set('port', (process.env.PORT || 8081)) // sets the port for the server to listen on
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use('/api', api) //makes the app use the api routes
+app.use(express.static('static')) //set up express to have access to static files
+
+app.use(morgan('dev')) // set up the app to do logging for development
+
+app.use(function(req,res,next){             //handles requests to unknown resources
+    const err = new Error('Not Found')
+    err.status = 404
+    res.json(err)
+})
+
+const mongoose = require('mongoose')        //handle to mongo db
+mongoose.connect('mongodb://localhost:27017/globomantics') //connection string should be an environment variable
+const db = mongoose.connection  //grab the connection and set to a varibale
+
+db.on('error', console.error.bind(console, 'connection error:')) // logs any connection errors
+db.once('open', function () {                                    // handles the connection
+  console.log('Connected to MongoDB')                            // log the successful connection
+
+  app.listen(app.get('port'), function () {                      // set the server app listening on the port
+    console.log('API Server Listening on port ' + app.get('port') + '!')    //log out a message that the app is now listening
+  })
+})
